0387fca55280b87a03f0a977ade6a568905ab64c
mikevgt <30626694+mikevgt@users.noreply.github.com>
2017-08-30 08:42:17 -0400

    Add files via upload

---
+++ ViewLettersNavigationController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewLettersNavigationController.swift
+//  Grocr
+//
+//  Created by Michael Vincent on 6/15/17.
+//  Copyright © 2017 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewLettersNavigationController: UINavigationController {
+  
+  
+  //let businessName = UILabel()
+  
+  // MARK: UIViewController Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    print("in viewlettersnavcontroller 1")
+    
+    //let nextVC = self.storyboard!.instantiateViewController(withIdentifier: "myViewGroupDetailController") as! ViewGroupDetailController
+    //nextVC.myImgView.image = UIImame(named: ImageArray[indexPath.row])
+    //print("in viewgroupnavcontroller 1")
+    
+    
+    //nextVC.businessName.text = businessName.text
+    
+    //print("in viewgroupnavcontroller 1")
+    
+  }
+  
+}
